828f0e3addba98807005a801562b9c5a35824b86
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2023-04-20 14:10:34 -0300

    Primeiro componente de teste: VButton

---
+++ src/components/VButton.vue
@@ -0,0 +1,6 @@
+<template>
+  Mama mia:
+  <button v-bind="$attrs">
+    <slot />
+  </button>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import { resolve } from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -10,5 +12,22 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/components/index.js'),
+      name: 'crosierfrt-core',
+      fileName: (format) => `crosierfrt-core.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // Add external deps here
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 })
