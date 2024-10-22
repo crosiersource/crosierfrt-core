4194302fa8f54abda64aa39cff38e05f333f34ee
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2024-10-22 19:59:07 -0300

    Excluindo tudo para reaplicar o ultima.

---
+++ src/main.bkp.js
@@ -0,0 +1,42 @@
+import { createApp, markRaw } from 'vue';
+import App from './App.vue';
+import router from './router';
+
+import { createPinia } from 'pinia';
+
+import BlockViewer from '@/components/BlockViewer.vue';
+import Aura from '@primevue/themes/aura';
+import PrimeVue from 'primevue/config';
+import ConfirmationService from 'primevue/confirmationservice';
+import ToastService from 'primevue/toastservice';
+
+import '@/assets/styles.scss';
+import '@/assets/tailwind.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+const app = createApp(App);
+
+app.use(router);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: '.app-dark'
+    }
+  }
+});
+
+const pinia = createPinia();
+
+pinia.use(({ store }) => {
+  store.$router = markRaw(router);
+});
+
+app.use(ToastService);
+app.use(ConfirmationService);
+
+app.use(pinia);
+
+app.component('BlockViewer', BlockViewer);
+
+app.mount('#app');
--- src/views/User.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div>User {{ $route.params.id }}</div>
-</template>
